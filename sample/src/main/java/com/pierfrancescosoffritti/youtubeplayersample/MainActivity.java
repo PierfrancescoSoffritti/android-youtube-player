62f4e1997a11e6dfba911aab375b44822faac26f
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-02-26 11:36:27 +0100

    refactored sample app

--- sample/src/main/java/com/pierfrancescosoffritti/youtubeplayersample/MainActivity.java
+++ sample/src/main/java/com/pierfrancescosoffritti/youtubeplayersample/MainActivity.java
@@ -8,12 +8,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.Video;
-import com.google.api.services.youtube.model.VideoListResponse;
 import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayer;
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerFullScreenListener;
@@ -21,20 +17,15 @@ import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerView;
 import com.pierfrancescosoffritti.youtubeplayer.ui.PlayerUIController;
 import com.pierfrancescosoffritti.youtubeplayer.ui.menu.MenuItem;
 
-import java.io.IOException;
 import java.util.Random;
 
 import javax.annotation.Nullable;
 
 import io.reactivex.Single;
-import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String APP_NAME = "YouTubePlayer_SampleApp";
-    private static final String YOUTUBE_DATA_API_KEY = "AIzaSyAVeTsyAjfpfBBbUQq4E7jooWwtV2D_tjE";
 
     private YouTubePlayerView youTubePlayerView;
     private FullScreenManager fullScreenManager;
@@ -43,18 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
     private Button nextVideoButton;
 
-    // the last video is not playable in some countries. It's here to test what happens for non-playable videos.
-    private String[] videoIds = {"6JYIGclVQdw", "LvetJ9U_tVY", "sop2V_MREEI"};
+    private String[] videoIds = {"6JYIGclVQdw", "LvetJ9U_tVY"};
+
+    // a list of videos not available in some countries, to test if they're handled gracefully.
+    private String[] nonPlayableVideoIds = { "sop2V_MREEI" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nextVideoButton = findViewById(R.id.next_video_button);
-
         fullScreenManager = new FullScreenManager(this);
 
+        nextVideoButton = findViewById(R.id.next_video_button);
         youTubePlayerView = findViewById(R.id.youtube_player_view);
 
         this.getLifecycle().addObserver(youTubePlayerView);
@@ -72,20 +64,22 @@ public class MainActivity extends AppCompatActivity {
             });
 
             addFullScreenListenerToPlayer(initializedYouTubePlayer);
-            initButtonClickListener(initializedYouTubePlayer);
+            setNextVideoButtonClickListener(initializedYouTubePlayer);
 
         }, true);
 
         // menu
         youTubePlayerView.getPlayerUIController().showMenuButton(true);
-        youTubePlayerView.getPlayerUIController().getMenu().addItem(new MenuItem("example", R.drawable.ic_settings_24dp, null));
+        youTubePlayerView.getPlayerUIController().getMenu().addItem(
+                new MenuItem("example", R.drawable.ic_settings_24dp, (view) -> Toast.makeText(this, "item clicked", Toast.LENGTH_SHORT).show())
+        );
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        // It's not necessary to call release if you register youTubePlayerView as a lifecycle observer of this Activity
+        // It's not necessary to call release if you register youTubePlayerView as a lifecycle observer of this Activity.
 //        youTubePlayerView.release();
     }
 
@@ -117,10 +111,7 @@ public class MainActivity extends AppCompatActivity {
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
                 fullScreenManager.enterFullScreen();
 
-                Drawable icon = ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_pause_36dp);
-                youTubePlayerView.getPlayerUIController().setCustomAction1(icon, view -> {
-                    if(youTubePlayer != null) youTubePlayer.pause();
-                });
+                addCustomActionToPlayer(youTubePlayer);
             }
 
             @Override
@@ -128,14 +119,27 @@ public class MainActivity extends AppCompatActivity {
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                 fullScreenManager.exitFullScreen();
 
-                youTubePlayerView.getPlayerUIController().showCustomAction1(false);
+                removeCustomActionFromPlayer();
             }
         });
     }
 
-    private void initButtonClickListener(final YouTubePlayer youTubePlayer) {
+    private void addCustomActionToPlayer(YouTubePlayer youTubePlayer) {
+        Drawable icon = ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_pause_36dp);
+
+        youTubePlayerView.getPlayerUIController().setCustomAction1(icon, view -> {
+            if(youTubePlayer != null) youTubePlayer.pause();
+        });
+    }
+
+    private void removeCustomActionFromPlayer() {
+        youTubePlayerView.getPlayerUIController().showCustomAction1(false);
+    }
+
+    private void setNextVideoButtonClickListener(final YouTubePlayer youTubePlayer) {
         nextVideoButton.setOnClickListener(view -> {
             String videoId = videoIds[new Random().nextInt(videoIds.length)];
+
             youTubePlayer.loadVideo(videoId, 0);
             setVideoTitle(youTubePlayerView.getPlayerUIController(), videoId);
         });
@@ -143,15 +147,18 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called every time a new video is being loaded/cued.
-     * It uses the YouTube Data APIs to get the video title from the video ID. You can learn more here https://developers.google.com/youtube/v3/docs/videos/list and here https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.videos.list?part=snippet&id=6JYIGclVQdw&fields=items(snippet(title))&_h=9&
+     * It uses the YouTube Data APIs to fetch the video title from the video ID.
      * The YouTube Data APIs are nothing more then a wrapper over the YouTube REST API.
+     * You can learn more at the following urls:
+     * https://developers.google.com/youtube/v3/docs/videos/list
+     * https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.videos.list?part=snippet&id=6JYIGclVQdw&fields=items(snippet(title))&_h=9&
      *
-     * youTubeDataAPIEndPoint.execute() does network operations, therefore it cannot be executed on the main thread.
-     * For simplicity I have used RxJava to implement the asynchronous logic. You can you whatever you want: Threads, AsyncTask ecc.
+     * This method does network operations, therefore it cannot be executed on the main thread.
+     * For simplicity I have used RxJava to implement the asynchronous logic. You can use whatever you want: Threads, AsyncTask ecc.
      */
     private void setVideoTitle(PlayerUIController playerUIController, String videoId) {
 
-        Single<String> observable = getVideoTitleFromYouTubeDataAPIs(videoId);
+        Single<String> observable = YouTubeDataEndpoint.getVideoTitleFromYouTubeDataAPIs(videoId);
 
         observable
                 .subscribeOn(Schedulers.io())
@@ -160,36 +167,5 @@ public class MainActivity extends AppCompatActivity {
                         videoTitle -> playerUIController.setVideoTitle(videoTitle),
                         error -> { Log.e(getClass().getSimpleName(), "Can't retrieve video title, are you connected to the internet?"); }
                 );
-    }
-
-    private Single<String> getVideoTitleFromYouTubeDataAPIs(String videoId) {
-        SingleOnSubscribe<String> onSubscribe = emitter -> {
-            try {
-                YouTube youTubeDataAPIEndPoint = new YouTube
-                        .Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                        .setApplicationName(APP_NAME)
-                        .build();
-
-                VideoListResponse videoListResponse = youTubeDataAPIEndPoint
-                        .videos()
-                        .list("snippet")
-                        .setFields("items(snippet(title))")
-                        .setId(videoId)
-                        .setKey(YOUTUBE_DATA_API_KEY)
-                        .execute();
-
-                if(videoListResponse.getItems().size() != 1)
-                    throw new RuntimeException("There should be exactly one video with the provided id");
-
-                Video video = videoListResponse.getItems().get(0);
-                String videoTitle = video.getSnippet().getTitle();
-                emitter.onSuccess(videoTitle);
-
-            } catch (IOException e) {
-                emitter.onError(e);
-            }
-        };
-
-        return Single.create(onSubscribe);
     }
 }
---
+++ sample/src/main/java/com/pierfrancescosoffritti/youtubeplayersample/YouTubeDataEndpoint.java
@@ -0,0 +1,59 @@
+package com.pierfrancescosoffritti.youtubeplayersample;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoListResponse;
+
+import java.io.IOException;
+
+import io.reactivex.Single;
+import io.reactivex.SingleOnSubscribe;
+
+class YouTubeDataEndpoint {
+
+    private static final String APP_NAME = "YouTubePlayer_SampleApp";
+    private static final String YOUTUBE_DATA_API_KEY = "AIzaSyAVeTsyAjfpfBBbUQq4E7jooWwtV2D_tjE";
+
+    static Single<String> getVideoTitleFromYouTubeDataAPIs(String videoId) {
+        SingleOnSubscribe<String> onSubscribe = emitter -> {
+            try {
+
+                YouTube youTubeDataAPIEndpoint = buildYouTubeEndpoint();
+
+                YouTube.Videos.List query = buildQuery(youTubeDataAPIEndpoint, videoId);
+                VideoListResponse videoListResponse = query.execute();
+
+                if(videoListResponse.getItems().size() != 1)
+                    throw new RuntimeException("There should be exactly one video with the specified id");
+
+                Video video = videoListResponse.getItems().get(0);
+                String videoTitle = video.getSnippet().getTitle();
+
+                emitter.onSuccess(videoTitle);
+
+            } catch (IOException e) {
+                emitter.onError(e);
+            }
+        };
+
+        return Single.create(onSubscribe);
+    }
+
+    private static YouTube.Videos.List buildQuery(YouTube youTubeDataAPIEndpoint, String videoId) throws IOException {
+        return youTubeDataAPIEndpoint
+                .videos()
+                .list("snippet")
+                .setFields("items(snippet(title))")
+                .setId(videoId)
+                .setKey(YOUTUBE_DATA_API_KEY);
+    }
+
+    private static YouTube buildYouTubeEndpoint() {
+        return new YouTube
+                .Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                .setApplicationName(APP_NAME)
+                .build();
+    }
+}
