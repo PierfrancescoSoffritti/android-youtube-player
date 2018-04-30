f35e832168e0f265b41611220af37263a5149eca
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-04-30 17:10:43 +0200

    player status example

---
+++ sample/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayersample/examples/playerStatusExample/PlayerStatusExampleActivity.java
@@ -0,0 +1,20 @@
+package com.pierfrancescosoffritti.androidyoutubeplayersample.examples.playerStatusExample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.pierfrancescosoffritti.androidyoutubeplayersample.R;
+
+public class PlayerStatusExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_status_example);
+    }
+
+}
