1de611f52c9711f6d40d52ddd0f07ff5e2a1d79f
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-05-13 13:05:38 +0200

    picture in picture example, solved issue #127

--- sample/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayersample/examples/pictureInPictureExample/PictureInPictureActivity.java
+++ sample/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayersample/examples/pictureInPictureExample/PictureInPictureActivity.java
@@ -1,51 +1,85 @@
 package com.pierfrancescosoffritti.androidyoutubeplayersample.examples.pictureInPictureExample;
 
+import android.arch.lifecycle.Lifecycle;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import com.pierfrancescosoffritti.androidyoutubeplayersample.R;
+import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayer;
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerView;
 
 public class PictureInPictureActivity extends AppCompatActivity {
 
-    YouTubePlayerView youTubePlayerView;
+    private YouTubePlayerView youTubePlayerView;
+    private String[] videoIds = {"6JYIGclVQdw"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_in_picture_example);
 
+        initYouTubePlayerView();
+    }
+
+    private void initYouTubePlayerView() {
         youTubePlayerView = findViewById(R.id.youtube_player_view);
+        youTubePlayerView.getPlayerUIController().showFullscreenButton(false);
+
         getLifecycle().addObserver(youTubePlayerView);
+        initPictureInPicture(youTubePlayerView);
 
-        Button pipButton = new Button(this);
+        youTubePlayerView.initialize(youTubePlayer -> {
+            youTubePlayer.addListener(new AbstractYouTubePlayerListener() {
+                @Override
+                public void onReady() {
+                    loadVideo(youTubePlayer, videoIds[0]);
+                }
+            });
+        }, true);
+    }
 
-        pipButton.setOnClickListener( view -> {
+    private void initPictureInPicture(YouTubePlayerView youTubePlayerView) {
+        ImageView pictureInPictureView = new ImageView(this);
+        pictureInPictureView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_picture_in_picture_24dp));
+
+        pictureInPictureView.setOnClickListener( view -> {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
                 enterPictureInPictureMode();
-            else
-                Log.e(getClass().getSimpleName(), "can't access pip, SDK required to be >= N");
+            else {
+                new AlertDialog.Builder(this)
+                        .setTitle("Can't enter picture in picture mode")
+                        .setMessage("In order to enter picture in picture mode you need a SDK version >= N.")
+                        .show();
+            }
         });
 
-        youTubePlayerView.getPlayerUIController().addView( pipButton );
+        youTubePlayerView.getPlayerUIController().addView( pictureInPictureView );
+    }
+
+    private void loadVideo(YouTubePlayer youTubePlayer, String videoId) {
+        if(getLifecycle().getCurrentState() == Lifecycle.State.RESUMED)
+            youTubePlayer.loadVideo(videoId, 0);
+        else
+            youTubePlayer.cueVideo(videoId, 0);
     }
 
     @Override
     public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
         super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
 
-        if(isInPictureInPictureMode)
+        if(isInPictureInPictureMode) {
             youTubePlayerView.enterFullScreen();
-        else
+            youTubePlayerView.getPlayerUIController().showUI(false);
+        } else {
             youTubePlayerView.exitFullScreen();
+            youTubePlayerView.getPlayerUIController().showUI(true);
+        }
     }
 }
