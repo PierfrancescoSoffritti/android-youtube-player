67efcd4bb77c7940d07e5ca46e4f11444807bbeb
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-06-17 08:33:13 +0200

    improved kotlin compatibility

--- YouTubePlayer/src/main/java/com/pierfrancescosoffritti/youtubeplayer/utils/YouTubePlayerStateTracker.java
+++ YouTubePlayer/src/main/java/com/pierfrancescosoffritti/youtubeplayer/utils/YouTubePlayerStateTracker.java
@@ -1,6 +1,7 @@
 package com.pierfrancescosoffritti.youtubeplayer.utils;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.youtubeplayer.player.PlayerConstants;
@@ -50,7 +51,7 @@ public class YouTubePlayerStateTracker extends AbstractYouTubePlayerListener{
         return videoDuration;
     }
 
-    public String getVideoId() {
+    public @Nullable String getVideoId() {
         return videoId;
     }
 }
