ffac1d1244deea3dfb5b09bad3fd5bac09e24e13
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2018-06-26 15:54:38 +0200

    documentation

--- chromecast-sender/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/chromecastsender/utils/PlayServicesUtils.kt
+++ chromecast-sender/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/chromecastsender/utils/PlayServicesUtils.kt
@@ -4,10 +4,13 @@ import android.app.Activity
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 
-/**
- * We can't use cast functionalities if GooglePlay Services aren't available, this utility class is used to check if GooglePlay Services are available and up to date.
- */
 object PlayServicesUtils {
+    /**
+     * It's not safe to use Google Cast functionalities if GooglePlay Services aren't available,
+     * this utility function is used to check if GooglePlay Services are available and up to date.
+     *
+     * The answer will be available in onActivityResult.
+     */
     @JvmStatic fun checkGooglePlayServicesAvailability(activity: Activity, googlePlayServicesAvailabilityRequestCode: Int, onSuccess: () -> Unit) {
         val googlePlayServicesAvailabilityResult = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(activity)
         if(googlePlayServicesAvailabilityResult == ConnectionResult.SUCCESS)
