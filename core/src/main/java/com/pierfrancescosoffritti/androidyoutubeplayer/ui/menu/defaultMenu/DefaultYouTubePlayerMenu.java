8b91e9a56d5d209e4b1cc07e48c472c6dfa89e55
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2019-01-17 07:26:48 +0100

    solves issue #260

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/ui/menu/defaultMenu/DefaultYouTubePlayerMenu.java
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/ui/menu/defaultMenu/DefaultYouTubePlayerMenu.java
@@ -35,7 +35,7 @@ public class DefaultYouTubePlayerMenu implements YouTubePlayerMenu {
     @Override
     public void show(View anchorView) {
         popupWindow = createPopupWindow();
-        popupWindow.showAsDropDown(anchorView, 0, - context.getResources().getDimensionPixelSize(R.dimen._8dp) * 4);
+        popupWindow.showAsDropDown(anchorView, - context.getResources().getDimensionPixelSize(R.dimen._8dp) * 12, - context.getResources().getDimensionPixelSize(R.dimen._8dp) * 12);
 
         if(menuItems.size() == 0)
             Log.e(YouTubePlayerMenu.class.getName(), "The menu is empty");
