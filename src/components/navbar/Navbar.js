785fc4d7d8f93a6922536b77b5ba9cae0aba4e26
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2019-01-22 17:27:58 +0100

    improves navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,8 +4,8 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dim">
-            <button className="navbar-item navbar-item-dim">android-youtube-player</button>
-            <div className="fill-space"></div>
+            <button className="navbar-item navbar-item-dim navbar-item-not-important">android-youtube-player</button>
+            <div className="fill-space navbar-item-not-important"></div>
             <button className="navbar-item navbar-item-dim">Documentation</button>
             <button className="navbar-item navbar-item-dim">Support</button>
             <button className="navbar-item navbar-item-dim">Open on GitHub</button>
