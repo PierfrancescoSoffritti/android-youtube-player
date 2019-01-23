c24c67e73c999b5266c95e7d6aa4dd2eb9f081e5
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2019-01-23 16:19:18 +0100

    Updates

---
+++ precache-manifest.b3047305422cb80fcc5f23f51365dd90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9c46bd2962533ffc9e7",
+    "url": "/android-youtube-player/static/js/runtime~main.d9c46bd2.js"
+  },
+  {
+    "revision": "95ab5c8207ac2b731097",
+    "url": "/android-youtube-player/static/js/main.95ab5c82.chunk.js"
+  },
+  {
+    "revision": "b88ef1372d59f3cc184f",
+    "url": "/android-youtube-player/static/js/1.b88ef137.chunk.js"
+  },
+  {
+    "revision": "95ab5c8207ac2b731097",
+    "url": "/android-youtube-player/static/css/main.5e804dfd.chunk.css"
+  },
+  {
+    "revision": "82720e54306d7cbe1ea8ff78e3c54323",
+    "url": "/android-youtube-player/index.html"
+  }
+];
