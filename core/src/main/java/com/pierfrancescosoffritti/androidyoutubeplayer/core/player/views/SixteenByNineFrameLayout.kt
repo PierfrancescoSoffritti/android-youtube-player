240d86193ac8bf2faadab8a9f66a6428279bf974
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-02-17 12:10:42 +0300

    Fix bug that prevented YouTubePlayerView from receiving click events (#521)
    This pull requests fixes a bug that prevented users from registering onClick/onLongCLick/onTouch listeners on YouTubePlayerView
    
    Test: YouTubePlayerViewClickTest.kt
    Bug: #105

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/SixteenByNineFrameLayout.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/SixteenByNineFrameLayout.kt
@@ -2,6 +2,7 @@ package com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
@@ -16,11 +17,37 @@ open class SixteenByNineFrameLayout: FrameLayout {
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr)
 
+    private var customOnTouchListener: OnTouchListener? = null
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         if (layoutParams.height == ViewGroup.LayoutParams.WRAP_CONTENT) {
             val sixteenNineHeight = View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.getSize(widthMeasureSpec) * 9 / 16, View.MeasureSpec.EXACTLY)
             super.onMeasure(widthMeasureSpec, sixteenNineHeight)
         } else
             super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    /**
+     * This method is usually used to intercept events.
+     * This allows to watch events as they are dispatched to the children,
+     * and take ownership of the current gesture at any point.
+     *
+     * We're using it to call onTouchEvent() without stopping
+     * the event from reaching the children.
+     */
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+
+        // Even though click and long click events are being called, the touch listener
+        // doesn't seem to work. So we need to register and use our own touch listener
+       val isTouchEventHandled = customOnTouchListener?.onTouch(rootView,ev) ?: false
+
+        if(!isTouchEventHandled)
+            onTouchEvent(ev)
+
+        return super.onInterceptTouchEvent(ev)
+    }
+
+    override fun setOnTouchListener(listener: OnTouchListener?) {
+        customOnTouchListener = listener
     }
 }
