ae5db1718b438b1a93e21fc0dfc4209746b2fe4a
anjalsaneen <anjal@okcredit.in>
2021-01-29 08:09:24 +0530

    Removed sample apps

--- chromecast-sender-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/sampleapp/examples/localPlayerExample/MediaRouteButtonContainer.kt
+++
@@ -1,8 +0,0 @@
-package com.pierfrancescosoffritti.androidyoutubeplayer.chromecast.sampleapp.examples.localPlayerExample
-
-import androidx.mediarouter.app.MediaRouteButton
-
-interface MediaRouteButtonContainer {
-    fun addMediaRouteButton(mediaRouteButton: MediaRouteButton)
-    fun removeMediaRouteButton(mediaRouteButton: MediaRouteButton)
-}
--- chromecast-sender-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/sampleapp/utils/VideoInfo.kt
+++
@@ -1,5 +0,0 @@
-package com.pierfrancescosoffritti.androidyoutubeplayer.chromecast.sampleapp.utils
-
-import android.graphics.Bitmap
-
-data class VideoInfo(val videoTitle: String, val channelTitle: String, val thumbnail: Bitmap?)
