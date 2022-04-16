ab0d8ab65945bc3e4e629f55df7f232744fd4df8
JosiYosi <36492260+JosiYosi@users.noreply.github.com>
2022-04-16 16:07:55 -0400

    Update TimeUtilities.kt
    Video playing elapsed time and video total duration was formatted only in minutes and seconds. Now for an hours long video, it formatted in hours, minutes and seconds.

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/ui/utils/TimeUtilities.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/ui/utils/TimeUtilities.kt
@@ -10,8 +10,27 @@ object TimeUtilities {
     @SuppressLint("DefaultLocale")
     @JvmStatic
     fun formatTime(timeInSeconds: Float): String {
-        val minutes = (timeInSeconds / 60).toInt()
-        val seconds = (timeInSeconds % 60).toInt()
-        return String.format("%d:%02d", minutes, seconds)
+        var timeString =""
+        
+        var hours = timeInSeconds / 3600
+        var minutes = (timeInSeconds % 3600) / 60
+        var seconds = timeInSeconds % 60
+       
+        
+         //formatting hours minutes and seconds in string
+        if(hours==0L && minutes==0L){
+            timeString = String.format("%d:%02d", minutes, seconds)
+        }else if(hours==0L && minutes<10L){
+            timeString = String.format("%d:%02d", minutes, seconds)
+        }else if(hours==0L && minutes>9L){
+            timeString = String.format("%02d:%02d", minutes, seconds)
+        }else if(hours<10L && hours>0L){
+            timeString = String.format("%d:%02d:%02d", hours, minutes, seconds)
+        }else if(hours>9L){
+            timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        }
+
+        
+        return timeString
     }
 }
