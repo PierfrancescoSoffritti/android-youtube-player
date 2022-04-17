46a1922dfa24ec254746d7598f91cea5f2c9c96f
JosiYosi <36492260+JosiYosi@users.noreply.github.com>
2022-04-17 18:36:07 -0400

    Update TimeUtilities.kt
    Converting float to long, and then adding hours format for hours long videos.

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/ui/utils/TimeUtilities.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/ui/utils/TimeUtilities.kt
@@ -10,8 +10,29 @@ object TimeUtilities {
     @SuppressLint("DefaultLocale")
     @JvmStatic
     fun formatTime(timeInSeconds: Float): String {
-        val minutes = (timeInSeconds / 60).toInt()
-        val seconds = (timeInSeconds % 60).toInt()
-        return String.format("%d:%02d", minutes, seconds)
+        var timeString =""
+        
+        var timeInLong = timeInSeconds.toLong()//convert float to Long
+        
+        var hours = timeInLong / 3600
+        var minutes = (timeInLong % 3600) / 60
+        var seconds = timeInLong % 60
+
+
+         //formatting hours minutes and seconds in string
+        if(hours==0L && minutes==0L){
+            timeString = String.format("%d:%02d", minutes, seconds)
+        }else if(hours==0L && minutes<10L){
+            timeString = String.format("%d:%02d", minutes, seconds)
+        }else if(hours==0L && minutes>9L){
+            timeString = String.format("%02d:%02d", minutes, seconds)
+        }else if(hours<10L && hours>0L){
+            timeString = String.format("%d:%02d:%02d", hours, minutes, seconds)
+        }else if(hours>9L){
+            timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        }
+
+
+        return timeString
     }
 }
