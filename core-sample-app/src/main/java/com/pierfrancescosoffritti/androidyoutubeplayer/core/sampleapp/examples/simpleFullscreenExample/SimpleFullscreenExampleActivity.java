f9df9cfe91dffd7b68def632b27f44b7052aacef
Praveen-Pable <praveen.developer.pable@gmail.com>
2023-02-19 19:19:00 +0530

    sample full screen example using state change handle

--- core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/examples/simpleFullscreenExample/SimpleFullscreenExampleActivity.java
+++ core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/examples/simpleFullscreenExample/SimpleFullscreenExampleActivity.java
@@ -4,9 +4,10 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions;
@@ -16,6 +17,7 @@ import com.pierfrancescosoffritti.aytplayersample.R;
 public class SimpleFullscreenExampleActivity extends AppCompatActivity {
 
     private YouTubePlayerView youTubePlayerView;
+    private FrameLayout customView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +30,46 @@ public class SimpleFullscreenExampleActivity extends AppCompatActivity {
                 .build();
 
         youTubePlayerView = findViewById(R.id.youtube_player_view);
+        customView = findViewById(R.id.customView);
 
         youTubePlayerView.setEnableAutomaticInitialization(false);
         youTubePlayerView.initialize(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onStateChange(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerState state, @NonNull View view) {
+                switch (state) {
+                    case SHOW_CUSTOM_VIEW: {
+                        // for display full screen landscape view
+                        youTubePlayerView.setVisibility(View.GONE);
+                        customView.setVisibility(View.VISIBLE);
+                        customView.addView(view);
+                        if (!youTubePlayerView.isFullScreen()) {
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                        }
+                        break;
+                    }
+
+                    case HIDE_CUSTOM_VIEW: {
+                        // for display normal portrait view
+                        youTubePlayerView.setVisibility(View.VISIBLE);
+                        customView.setVisibility(View.GONE);
+                        if (youTubePlayerView.isFullScreen()) {
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                        }
+                        break;
+                    }
+
+                    default: {
+                        break;
+                    }
+                }
+            }
+
             @Override
             public void onReady(@NonNull YouTubePlayer youTubePlayer) {
                 super.onReady(youTubePlayer);
                 youTubePlayer.loadVideo("S0Q4gqBUs7c", 0F);
             }
         }, iFramePlayerOptions);
-
-        View viewButtonFullScreen = findViewById(R.id.view_button_fullscreen);
-        viewButtonFullScreen.setOnClickListener(view -> {
-            if (youTubePlayerView.isFullScreen()) {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-            } else {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-            }
-
-        });
 
         getLifecycle().addObserver(youTubePlayerView);
     }
