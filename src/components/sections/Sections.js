9728982fdf548aca5c8971b896c263e9f81ca002
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2023-03-17 08:15:46 +0000

    Add users description

--- src/components/sections/Sections.js
+++ src/components/sections/Sections.js
@@ -2,6 +2,7 @@ import React from 'react';
 import GhButtons from './gh-buttons/GhButtons';
 import WhatIsIt from './whatIsIt/WhatIsIt';
 import Differences from './differences/Differences';
+import Users from './users/Users';
 import Download from './download/Download';
 import GettingStarted from './gettingStarted/GettingStarted';
 import SampleApps from './sampleApps/SampleApps';
@@ -13,6 +14,7 @@ const Sections = () => {
             <section className="gh-buttons-section-dim"><GhButtons /></section>
             <section className="section-dim"><WhatIsIt /></section>
             <section className="section-dim"><Differences /></section>
+            <section className="section-dim"><Users /></section>
             <section className="section-dim"><Download /></section>
             <section className="section-dim"><GettingStarted /></section>
             <section className="last-section-dim"><SampleApps /></section>
---
+++ src/components/sections/users/Users.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Users.css"
+
+const Users = () => {
+    return (
+        <section>
+            <div className="section-title">Who is using this library</div>
+            <div>
+            Now that the official API from Google is deprecated, android-youtube-player is the main YouTube player library for Android.
+            <br/><br/>
+            <b>Used by over 5 thousands apps</b>, with some big names like <a href="https://play.google.com/store/apps/details?id=com.flipkart.android" target="_blank" rel="noopener noreferrer">Flipkart</a>, <a href="https://play.google.com/store/apps/details?id=com.mcdo.mcdonalds" target="_blank" rel="noopener noreferrer">McDonald's</a>, <a href="https://play.google.com/store/apps/details?id=com.camerasideas.instashot" target="_blank" rel="noopener noreferrer">InShot Video Editor</a>, <a href="https://play.google.com/store/apps/details?id=com.genius.android" target="_blank" rel="noopener noreferrer">Genius</a> and <a href="https://play.google.com/store/apps/details?id=com.andrewshu.android.reddit" target="_blank" rel="noopener noreferrer">reddit is fun</a>.
+            <br/>
+            <a href="https://www.appbrain.com/stats/libraries/details/android_youtube_player/android-youtube-player" target="_blank" rel="noopener noreferrer">You can see more stats here</a>.
+            <br/><br/>
+            If you choose to use this library and profit from it, consider informing me and <a href="https://github.com/sponsors/PierfrancescoSoffritti" target="_blank" rel="noopener noreferrer">become a sponsor on GitHub</a>. This will enable me to continue developing the library, so you don't have to.
+            </div>
+        </section>
+    );
+}
+
+export default Users
