63362f669aa0b9266768f30d3deb11e6a22de872
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2023-03-17 08:00:12 +0000

    Update library description sections

--- src/components/sections/differences/Differences.js
+++ src/components/sections/differences/Differences.js
@@ -4,12 +4,14 @@ import "./Differences.css"
 const Differences = () => {
     return (
         <section>
-            <div className="section-title">Differences from the official API</div>
+            <div className="section-title">Why does this library exist?</div>
             <div>
-                The official library provided by Google to integrate YouTube videos in Android apps is the YouTube Android Player API.<br/>
-                Unfortunately this library is quite buggy (<a href="https://issuetracker.google.com/issues/35172585" target="_blank" rel="noopener noreferrer">some bugs are 5+ years old</a>) and has receive no updates in years. I personally found it quite unreliable and therefore impossible to use in production.<br/><br/>
-                This, added to its limited options for customization and lack of Chromecast support, lead to the development of this open source library.<br/><br/>
-                A lengthier explanation about why you may want to consider using an alternative to the official YouTube player is written <a href="https://medium.com/@soffritti.pierfrancesco/how-to-play-youtube-videos-in-your-android-app-c40427215230" target="_blank" rel="noopener noreferrer">in this Medium post</a>.
+            The library provided by Google is the YouTube Android Player API.
+            This library <a href="https://issuetracker.google.com/issues/35172585" target="_blank" rel="noopener noreferrer">has been historically not reliable</a> and is now deprecated by Google.
+            <br/><br/>
+            Google now recommends using the IFrame Player API inside a WebView, which is exactly what this library does, while also providing a native Java/Kotlin interface to interact with the web player.
+            <br/><br/>
+            A lengthier explanation of why this library was created can be found <a href="https://medium.com/@soffritti.pierfrancesco/how-to-play-youtube-videos-in-your-android-app-c40427215230" target="_blank" rel="noopener noreferrer">in this Medium post</a>.
             </div>
         </section>
     );
--- src/components/sections/whatIsIt/WhatIsIt.js
+++ src/components/sections/whatIsIt/WhatIsIt.js
@@ -8,7 +8,6 @@ const WhatIsIt = () => {
             <div className="text">
                 android-youtube-player is a stable and customizable open source YouTube player for Android. It provides a simple View that can be easily integrated in every Activity/Fragment.<br/><br/>
                 The library is a wrapper over the <a href="https://developers.google.com/youtube/iframe_api_reference" target="_blank" rel="noopener noreferrer">IFrame Player API</a>, which runs inside of a WebView.<br/><br/>
-                The UI of the player is 100% customizable. The UI can be <a href="https://github.com/PierfrancescoSoffritti/android-youtube-player#create-your-own-custom-ui" target="_blank" rel="noopener noreferrer">completely replaced by a custom UI</a>.<br/><br/>
                 This library also provides a <a href="https://github.com/PierfrancescoSoffritti/android-youtube-player#chromecast-extension-library" target="_blank" rel="noopener noreferrer">Chromecast YouTube player extension</a>, that can be used to cast YouTube videos from an Android app to a Chromecast device.
             </div>
         </section>
