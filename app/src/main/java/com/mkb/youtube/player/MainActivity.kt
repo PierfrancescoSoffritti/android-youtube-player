0ba7977b8475ce9a0b035105994fbe434db9a605
Jayesh kukadiya <kukadia.jayesh@gmail.com>
2023-05-05 22:07:00 +0530

    Added everything with sample

--- app/src/main/java/com/mkb/youtube/player/MainActivity.kt
+++ app/src/main/java/com/mkb/youtube/player/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.mkb.youtube.player
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mkb.youtube.core.player.views.YouTubePlayerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var youTubePlayerView: YouTubePlayerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        youTubePlayerView = findViewById(R.id.youtube_player_view)
+        lifecycle.addObserver(youTubePlayerView!!)
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            youTubePlayerView!!.matchParent()
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            youTubePlayerView!!.wrapContent()
+        }
     }
 }
---
+++ mkb-custom-ui/src/main/java/com/mkb/youtube/core/customui/menu/MenuItem.kt
@@ -0,0 +1,10 @@
+package com.mkb.youtube.core.customui.menu
+
+import android.view.View
+import androidx.annotation.DrawableRes
+
+data class MenuItem @JvmOverloads constructor(
+  val text: String,
+  @DrawableRes val icon: Int? = null,
+  val onClickListener: View.OnClickListener
+)
