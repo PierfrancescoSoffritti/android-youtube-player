06520d5b1d5c0fddaf4d3b822b9e11d01720fc52
santi <santi.detord@posteo.net>
2023-10-26 18:58:00 +0200

    remove redundant toFloat call

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/WebViewYouTubePlayer.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/WebViewYouTubePlayer.kt
@@ -46,7 +46,7 @@ private class YouTubePlayerImpl(private val webView: WebView) : YouTubePlayer {
   }
   override fun seekTo(time: Float) = webView.invoke("seekTo", time)
   override fun setPlaybackRate(playbackRate: PlayerConstants.PlaybackRate) = setPlaybackRate(playbackRate.toFloat())
-  override fun setPlaybackRate(playbackRate: Float) = webView.invoke("setPlaybackRate", playbackRate.toFloat())
+  override fun setPlaybackRate(playbackRate: Float) = webView.invoke("setPlaybackRate", playbackRate)
   override fun toggleFullscreen() = webView.invoke("toggleFullscreen")
   override fun addListener(listener: YouTubePlayerListener) = listeners.add(listener)
   override fun removeListener(listener: YouTubePlayerListener) = listeners.remove(listener)
