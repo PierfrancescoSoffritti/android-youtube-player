7e244a64d31bee71e9e803e9d7798b95f199545a
Blagovest Dachev <blago@dachev.com>
2025-03-29 21:31:43 -0400

    Revert "Add YouTubePlayerStateProvider interface to query player state"
    This reverts commit b30dc843cdb5f9c11afb47bd85424f6fc2c55e8d.

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/YouTubePlayerStateProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.pierfrancescosoffritti.androidyoutubeplayer.core.player
-
-interface YouTubePlayerStateProvider {
-    /** Gets the current mute state of the player. Suspending function for Kotlin coroutine use. */
-    suspend fun isMuted(): Boolean
-
-    /** Provides the current mute state of the player via callback for Java compatibility. */
-    fun isMuted(callback: (Boolean) -> Unit)
-}
