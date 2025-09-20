b87558b64a0983ae308599c2db4c4ffe7ac9a60f
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2025-09-20 17:17:56 +0100

    Remove youtube.com from default origin
    As discussed in #1252, the default origin can't be "https://www.youtube.com" anymore. With this origin the IFrame Player API will reject the request and fail to play the video.
    
    According to the updated IFrame player documentation, the origin should ideally be set to "https://[app-id]", where app-id is the id of the app.
    
    From within the library we don't know the app-id of the client app, therefore the default origin value is now set to "https://com.pierfrancescosoffritti.androidyoutubeplayer".
    
    This should make no different from YouTube point's of view. Client can set their own app-id by providing an IFramePlayerOptions to the YouTubePlayerView initialization method.

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/options/IFramePlayerOptions.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/options/IFramePlayerOptions.kt
@@ -47,7 +47,7 @@ class IFramePlayerOptions private constructor(private val playerOptions: JSONObj
       addInt(CONTROLS, 0)
       addInt(ENABLE_JS_API, 1)
       addInt(FS, 0)
-      addString(ORIGIN, "https://www.youtube.com")
+      addString(ORIGIN, "https://com.pierfrancescosoffritti.androidyoutubeplayer")
       addInt(REL, 0)
       addInt(IV_LOAD_POLICY, 3)
       addInt(CC_LOAD_POLICY, 0)
